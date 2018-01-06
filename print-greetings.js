ad282b82400f32b466104591f2b85c9994f5d82a
ParthKundaria <parth.kundaria@gmail.com>
2018-01-06 23:51:25 +0530

    Initial

---
+++ print-greetings.js
@@ -0,0 +1,6 @@
+//var greetings = require('./greetings.js')
+var greetings = require('./greetingsv2.js')
+
+console.log('Swedish ' + greetings.sayHelloInSwedish() +
+'\nEnglish ' + greetings.sayHelloInEnglish() +
+'\nTatar ' + greetings.sayHelloInTatar())
