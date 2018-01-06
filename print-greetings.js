cfbaa632d06f85b6c574b8b8e823d35adda42728
ParthKundaria <34009013+ParthKundaria@users.noreply.github.com>
2018-01-06 23:42:43 +0530

    Delete print-greetings.js

--- print-greetings.js
+++
@@ -1,6 +0,0 @@
-//var greetings = require('./greetings.js')
-var greetings = require('./greetingsv2.js')
-
-console.log('Swedish ' + greetings.sayHelloInSwedish() +
-'\nEnglish ' + greetings.sayHelloInEnglish() +
-'\nTatar ' + greetings.sayHelloInTatar())
